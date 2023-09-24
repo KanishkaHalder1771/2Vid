3f0fc70b97a38e01b0205e6579885da2ce0f909a
AniruddhaChattopadhyay <studyaniruddha@gmail.com>
2023-09-24 18:01:42 +0530

    Adding the landing Page Code here

---
+++ 2vid_landing_page/run_at_reboot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pm2 start "npm run dev" --name 2vid_frontend --watch
---
+++ 2vid_landing_page/static/strings.js
@@ -0,0 +1,52 @@
+const strings = {
+    heroTitle: '2Vid : AI Enabled Seamless content creation',
+    heroSubHeading: 'Helping you realize your dream of becoming a content creator',
+    heroTrialButton: 'Start free trial',
+    heroLearnMoreButtom: 'Learn more',
+
+// Features Section
+    featTitle: 'Create videos in an instant',
+    featSubHeading: '10x your social footprint by using 2vid',
+
+    feat1Title: 'Script-to-Video',
+    feat1Desc: 'create multiple videos from a script in multiple languages of your choosing',
+
+    feat2Title: 'Blog-to-Video',
+    feat2Desc: 'Read an interesting blog? Want to make a video about it to share with your followers? Click here.',
+
+    feat3Title: 'Long Video-to-Shorts',
+    feat3Desc: 'Watched a youtube video that you think needs to be made into a short video to have more reach? We got you covered.',
+
+    feat4Title: 'Tweet/Reddit-to-Video',
+    feat4Desc: 'Generate Webhooks attached to a twitter account/subreddits to automatically create short videos from popular tweets/ upvoted reddits',
+
+    feat5Title: 'Topic to Video',
+    feat5Desc: 'From a selected topic automatically generate viral script on that topic and create short video',
+
+    feat6Title: 'Social media funnel',
+    feat6Desc: 'Create a uploading funnel to automatically create and upload videos to social media platforms',
+
+// ZigZag Section
+    zigBanner: 'Reach goals that matter',
+    zigTitle: 'One product, unlimited solutions',
+    zigSubHeading: 'Onestop platform to get AI generate short format video content to boost your following',
+
+// Testimonials
+    testimonial1 : '— 2Vid lets me create short videos from my long youtube videos. This is immensly helpful as short videos helps immensely in channel discovery both for youtube and instagram.',
+    testimonial2 : '— 2Vid has enabled me to push a lot more content than I would be normally be able to. It is a masssive help enabling me to use go from script to videos in a matter of minutes',
+    testimonial3 : '— I love 2Vid\'s blog to video feature. I run a health channel in bengali and it has helped me immensely to go from a blog in english to a short video in ',
+
+
+//Footer    
+    footerQuote : "\"Short tiktok like videos are the next revolution in the social media space. They will be used for marketing, campaigning, infotainment and anything else you can think of\" ---- Gennady Vaynerchuk",
+
+
+//Links
+    scriptToVideoForm: "https://forms.gle/NzUtDfS33G4qsAp87",
+    linkTree: "https://linktr.ee/News_X"
+
+
+
+}
+
+export default strings;
